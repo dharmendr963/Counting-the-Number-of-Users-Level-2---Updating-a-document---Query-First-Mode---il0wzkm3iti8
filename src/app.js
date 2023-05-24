7039b55294c4bf93a0da56455912b92bbc30484a
Dharmendra Kumar <114353361+dharmendr963@users.noreply.github.com>
2023-05-24 04:04:49 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -24,6 +24,18 @@ app.get("/",async function(req,res){
     var count = 0;
 
     //Write you code here
+db.myCollection.find({ $nor: [{ age: { $lt: 18 } }, { age: { $gt: 30 } }] });
+```
+
+This will return all documents where the **`age`** field is not less than 18 or greater than 30.
+
+You can also use these logical operators in combination with other query conditions to create more complex queries. For example:
+
+```
+db.myCollection.find({ $and: [{ age: { $gte: 18 } }, { age: { $lte: 30 } }], gender: "male" });
+```
+
+This will return all documents where the **`age`** field is between 18 and 30 (inclusive) and the **`gender`** field is "male".
     //update count variable
 
     res.send(JSON.stringify(count));
